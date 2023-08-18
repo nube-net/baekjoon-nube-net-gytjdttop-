ed7487f62d3c0462cf7e5aee1cc867e2b6383504
nube-net <129709301+nube-net@users.noreply.github.com>
2023-08-18 18:57:56 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,33 @@
+import sys
+
+S = list(sys.stdin.readline().rstrip())
+
+stack = []  # 연산자
+result = []
+cnt = 0
+order = {'+': 2, '-': 2, '*': 1, '/': 1, '(': 3, ')': 3}
+
+for s in S:
+    if str.isalpha(s):
+        result.append(s)
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == ')':
+            while True:
+                tmp = stack.pop()
+                if tmp == '(':
+                    break
+                else:
+                    result.append(tmp)
+        else:
+            while stack:
+                if order[stack[-1]] <= order[s]:
+                    result.append(stack.pop())
+                else:
+                    break
+            stack.append(s)
+while stack:
+    result.append(stack.pop())
+
+print(''.join(result))
