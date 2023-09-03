a16759b2a5686466f3eadf0ea8d6e40f602254b9
nube-net <129709301+nube-net@users.noreply.github.com>
2023-09-03 23:59:39 +0900

    [Bronze I] Title: 최소공배수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(input())
+for _ in range(n):  
+    a,b = map(int, sys.stdin.readline().split())
+    if b>a :
+        tmp= b
+        b=a
+        a=tmp
+    c=a*b
+    while a%b != 0 :
+        tmp = a%b
+        a=b
+        b=tmp
+    print(c//b)
+
