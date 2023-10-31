6307db27b1bb79a6f9f890a5e2b1e69c7166ac13
nube-net <129709301+nube-net@users.noreply.github.com>
2023-10-31 22:57:40 +0900

    [Gold IV] Title: 지뢰찾기, Time: 44 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/9082. 지뢰찾기/지뢰찾기.py
+++ 백준/Gold/9082. 지뢰찾기/지뢰찾기.py
@@ -8,29 +8,4 @@ for __ in range(T):
     Q = []
     cnt = [int(i) for i in input().strip()]
     a = list(str(input().strip()))
-    ans = 0
-    for i in range(n):
-        if a[i] == "*":
-            for idx in (i-1,i,i+1):
-                if 0 <= idx < n:
-                    cnt[idx] -= 1
-            ans += 1
-    #
-    for i in range(n):
-        if a[i] == "#":
-            key = False
-            for idx in (i,i+1,i-1):
-                if 0 <= idx < n:
-                    if cnt[idx] <= 0:
-                        key = True
-            if key:
-                continue
-            for idx in (i,i+1,i-1):
-                if 0 <= idx < n:
-                    cnt[idx] -= 1
-            ans +=1
-            a[i] = "*"
-            
-    ans += sum(cnt)//2
-    print(ans)
-
+    print((sum(cnt)+2)//3)
