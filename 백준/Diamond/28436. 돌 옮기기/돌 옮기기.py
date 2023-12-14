d944bc4ce42184d8411becc2f018342c39c2ff01
nube-net <129709301+nube-net@users.noreply.github.com>
2023-12-14 14:06:34 +0900

    [Diamond V] Title: 돌 옮기기, Time: 148 ms, Memory: 111740 KB -BaekjoonHub

--- 백준/Diamond/28436. 돌 옮기기/돌 옮기기.py
+++ 백준/Diamond/28436. 돌 옮기기/돌 옮기기.py
@@ -1,36 +1,32 @@
 import sys
-inputF = sys.stdin.readline
+input = sys.stdin.readline
 
-T = int(inputF())
-
-for _ in range(T):  # 흰색 돌이 먼저 움직임
-    S = inputF().rstrip()
-    count = 0  # 이득 계산
-    empty = 0  # 빈 공간
+T = int(input())
+for _ in range(T):
+    S = input().rstrip()
+    cnt = 0
     result = 0
-    for i in range(len(S)-1, -1, -1):  # 맨 뒤에서 부터 탐색, 앞부터 움직일 시 그 뒤에 공간이 생김
-        rock = S[i]  # 현재 위치 상태
+    empty = False  
 
-        # 경우의 수 2가지(+1)
-        if rock == '.':  # 공간
-            empty = 1
-        else:  # 돌
-            empty = 0
+    for rock in reversed(S):
+        if rock == '.': 
+            empty = True
+        else:
+            empty = False
             if rock == 'B':
-                if count > 0:
-                    count = 0
+                if cnt >0:
+                    cnt = 0
                 else:
-                    count -= 1  # 흰 돌 기준
-            elif rock == 'W':
-                if count < 0:
-                    count = 0
+                    cnt -= 1
+            else:
+                if cnt < 0:
+                    cnt = 0
                 else:
-                    count += 1
-        result += empty*count
+                    cnt += 1
+        if empty:
+            result += cnt
 
-    # 마무리
-    if result > 0:  # 먼저 시작 이기에 이득이 1이상 이어야 함
+    if result > 0:
         print('WHITE')
     else:
         print('BLACK')
-
