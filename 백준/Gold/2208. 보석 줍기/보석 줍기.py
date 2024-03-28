42d073e7382f661c6869f4341fbcdc4b05f6a68e
nube-net <129709301+nube-net@users.noreply.github.com>
2024-03-28 16:32:18 +0900

    [Gold II] Title: 보석 줍기, Time: 148 ms, Memory: 114428 KB -BaekjoonHub

--- 백준/Gold/2208. 보석 줍기/보석 줍기.py
+++ 백준/Gold/2208. 보석 줍기/보석 줍기.py
@@ -3,19 +3,25 @@ input =sys.stdin.readline
 
 n, m = map(int,input().split())
 arr = [int(input()) for _ in range(n)]
-prefix,min_, max_ = [0]*(n+1), [0] * (n+1), [0] * (n+1)
-
-for i in range(n):  # 누적합 배열
+prefix = [0]*(n+1)
+min_ = [0] * (n+1)
+max_ = [0] * (n+1)
+for i in range(n):
     prefix[i+1] = prefix[i]+arr[i]
-
-min_[1], max_[n] = prefix[1], prefix[n]
-for i in range(2, n+1):  # i이전 최소값 저장
-    min_[i] = min(min_[i-1], prefix[i])
-for i in range(n-1, 0, -1):  # i이후 최대값 저장
+min_[1] = prefix[1]
+max_[n] = prefix[n]
+for i in range(2,n+1):
+    min_[i] = min(min_[i-1],prefix[i])
+for i in range(n-1,0,-1):
     max_[i] = max(max_[i + 1], prefix[i])
+#print(prefix)
+#print(min_)
+#print(max_)
+min_[0] = 0
+max_[0] = 0
 
 ans = [0]*(n+1)
-for i in range(m,n+1):  # i까지 최대 기댓값
-    ans[i] = max_[i] - min_[i-m]
+for i in range(m,n+1):
+    ans[i]=max_[i]-min_[i-m]
 
 print(max(ans))
