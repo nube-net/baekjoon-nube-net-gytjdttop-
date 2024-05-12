6581b1cf17dff47a8fd8f9b978d45ef308685239
nube-net <129709301+nube-net@users.noreply.github.com>
2024-05-13 08:43:51 +0900

    [Gold I] Title: 수열과 쿼리 17, Time: 1304 ms, Memory: 144188 KB -BaekjoonHub

---
+++ 백준/Gold/14438. 수열과 쿼리 17/수열과 쿼리 17.py
@@ -0,0 +1,56 @@
+import sys
+input = sys.stdin.readline
+
+def merge(left, right):
+    return min(left,right) ###
+
+def build(stree, node, left, right):
+    if left == right:
+        stree[node] = arr[left]
+        return stree[node]
+
+    mid = left + (right - left) // 2
+    l_val = build(stree, 2 * node, left, mid)
+    r_val = build(stree, 2 * node + 1, mid + 1, right)
+    stree[node] = merge(l_val, r_val)
+    return stree[node]
+
+def query(start, end, node, left, right):
+    if end < left or start > right:
+        return sys.maxsize ###
+
+    if start <= left and right <= end:
+        return stree[node]
+
+    mid = left + (right - left) // 2
+    l_val = query(start, end, 2 * node, left, mid)
+    r_val = query(start, end, 2 * node + 1, mid + 1, right)
+    return merge(l_val, r_val)
+
+def update(idx, val, node, left, right):
+    if idx < left or idx > right:
+        return stree[node]
+    if left == right:
+        stree[node] = val
+        return stree[node]
+
+    mid = left + (right - left) // 2
+    l_val = update(idx, val, 2 * node, left, mid)
+    r_val = update(idx, val, 2 * node + 1, mid + 1, right)
+    stree[node] = merge(l_val, r_val)
+    return stree[node]
+
+n= int(input())
+arr = [0] + list(map(int,input().split()))
+stree = [0] * (4 * (n + 1))
+m = int(input())
+build(stree, 1, 1, n)
+for _ in range(m):
+    x, a, b = map(int, input().split())
+    if x == 1:
+        update(a, b, 1, 1, n)
+    else:
+        if a > b:
+            a,b =b,a
+        print(query(a, b, 1, 1, n))
+
