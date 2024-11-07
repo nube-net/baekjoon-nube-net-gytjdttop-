89002d0fe237cc66baf026bb117bf32bd455f39d
nube-net <gytjdttop@naver.com>
2024-11-07 14:28:01 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1020 ms, Memory: 245140 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,41 +1,45 @@
+from collections import deque
 import sys
-inputF = sys.stdin.readline
+input = sys.stdin.readline
 
-n, m = map(int, inputF().split())
-Map = [list(inputF()) for _ in range(n)]
+n, m = map(int, input().split())
+Map = [list(input()) for _ in range(n)]
 visited = [[[False]*m for _ in range(n)] for _ in range(2)]
-Q = set()
-Q.add((0, 0, 0))
-cnt = 0
-
+Q = deque()
+Q.append((0, 0, 0))
+visited[0][0][0] = True
+cnt = 1
 dx = [-1, 1, 0, 0]  # 상 하 좌 우
 dy = [0, 0, -1, 1]
+if n+m ==2:
+    print(1)
+    exit()
 while Q:
-    cnt +=1
-    tmp = set()
-    while Q:
-        x, y, z = Q.pop()
-        visited[z][x][y] = True  # 체크
+    cnt += 1
+    l = len(Q)
+    for _ in range(l):
+        x, y, z = Q.popleft()
+
         for i in range(4):
             X, Y = x + dx[i], y + dy[i]
             if (X < 0) or (X >= n) or (Y < 0) or (Y >= m):  # 범위 초과
                 continue
-
             if visited[z][X][Y]:  # 방문 여부
                 continue
             else:
                 if Map[X][Y] == '0':  # 이동 가능 하면
-                    tmp.add((X, Y, z))  # Q 저장
+                    Q.append((X, Y, z))  # Q 저장
+                    visited[z][X][Y] = True
                 else:  # 벽 일때
                     if z == 0:  # 부술 수 있을 때
-                        if X + dx[i] < 0 or X + dx[i] >= n or Y+dy[i] < 0 or Y+dy[i] >= m:  # 범위 초과
-                            continue
+                        for j in range(4):
+                            if X + dx[j] < 0 or X + dx[j] >= n or Y+dy[j] < 0 or Y+dy[j] >= m:  # 범위 초과
+                                continue
 
-                        if not visited[1][X+dx[i]][Y+dy[i]]:  # 방문 안 했으면
-                            # check
-                            if Map[X+dx[i]][Y+dy[i]] == '0':
-                                tmp.add((X+dx[i], Y+dy[i], 1))
-    Q = set(tmp)
+                            if not visited[1][X+dx[j]][Y+dy[j]]:  # 방문 안 했으면
+                                if Map[X+dx[j]][Y+dy[j]] == '0':
+                                    Q.append((X+dx[j], Y+dy[j], 1))
+                                    visited[1][X+dx[j]][Y+dy[j]] = True
     if visited[0][n - 1][m - 1] or visited[1][n - 1][m - 1]:
         break
 
