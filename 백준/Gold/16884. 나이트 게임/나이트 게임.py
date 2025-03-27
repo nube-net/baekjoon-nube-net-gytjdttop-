3e9bb3c0c1d2bc2df9016dfd16ef3ef47e5aede3
nube-net <gytjdttop@naver.com>
2025-03-27 13:44:32 +0900

    [Gold III] Title: 나이트 게임, Time: 100 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/16884. 나이트 게임/나이트 게임.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n= int(input())
+    if n%2 == 1:
+        print("koosaga")
+    else:
+        print("cubelover")
