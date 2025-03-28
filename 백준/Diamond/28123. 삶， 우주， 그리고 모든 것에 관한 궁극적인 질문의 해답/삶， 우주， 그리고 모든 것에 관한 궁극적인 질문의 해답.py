b162bca6b62a9747137561268637e32c7668f157
nube-net <gytjdttop@naver.com>
2025-03-28 15:58:04 +0900

    [Diamond V] Title: 삶, 우주, 그리고 모든 것에 관한 궁극적인 질문의 해답, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Diamond/28123. 삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답/삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답.py
@@ -0,0 +1,19 @@
+n, k, x = map(int, input().split())
+arr =[0,0,-1,-1,-1,-2,-2,-2,-2,-2]
+if n==1:
+    print(0)
+elif 4<= x <=9:
+    print(n%k + n//k +arr[x])
+    
+else:
+    if n >=24:
+        print(n%k + n//k +arr[x])
+        exit()
+    ans = 0
+    tmp = 1
+    for i in range(n):
+        tmp *= 2
+        if str(tmp)[0] == "4":
+            ans += 1
+    print(ans)
+    
