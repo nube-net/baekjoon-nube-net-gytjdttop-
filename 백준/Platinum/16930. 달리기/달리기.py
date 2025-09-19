bf699eb62be6f925cd21c803738897e7a2b1be22
nube-net <gytjdttop@naver.com>
2025-09-19 14:05:44 +0900

    [Platinum III] Title: 달리기, Time: 448 ms, Memory: 195016 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.py
+++ 백준/Platinum/16930. 달리기/달리기.py
@@ -2,46 +2,29 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+# AC받은후 빠른코드 굼금해서 질문한 gpt코드
 n, m, k = map(int,input().split())
 a = [list(input().rstrip()) for _ in range(n)]
 s1, s2, d1, d2 = map(int,input().split())
 
+INF = 10**9
+dist = [[INF]*m for _ in range(n)]
+dist[s1-1][s2-1] = 0
+
 q = deque()
-visit = [[False for _ in range(m)] for _ in range(n)]
+q.append((s1-1, s2-1))
 D = [(1,0), (-1,0), (0,-1), (0,1)]
-ans = 0
-visit[s1-1][s2-1] = True
-q.append([s1-1,s2-1])
-tmp = set()  # visit 처리할 인덱스
+
 while q:
-    l = len(q)
-    ans += 1
-    for _ in range(l):
-        cur = q.popleft()
-        
-        for x,y in D:
-            X = int(cur[0])
-            Y = int(cur[1])
-            for i in range(1,k+1):
-                X += x
-                Y += y
-                if (X,Y,x,y) in tmp:
-                    break
-                if X <0 or Y < 0 or X >= n or Y>=m:
-                    break
-                if a[X][Y] == "#" or visit[X][Y]:
-                    break
-                if X == d1-1 and Y == d2-1:
-                    print(ans)
-                    exit()
-                tmp.add((X,Y,x,y))
-                q.append([X,Y])
-    while tmp:
-        cur = tmp.pop()
-        visit[cur[0]][cur[1]] = True
-    
-print(-1)
+    x, y = q.popleft()
+    for dx, dy in D:
+        for step in range(1, k+1):
+            nx, ny = x + dx*step, y + dy*step
+            if not (0 <= nx < n and 0 <= ny < m): break
+            if a[nx][ny] == "#": break
+            if dist[nx][ny] < dist[x][y] + 1: break  # 이미 더 좋은 경로가 있음
+            if dist[nx][ny] == dist[x][y] + 1: continue  # 같은 거리로 중복 방문 방지
+            dist[nx][ny] = dist[x][y] + 1
+            q.append((nx, ny))
 
-    
-
-
+print(-1 if dist[d1-1][d2-1] == INF else dist[d1-1][d2-1])
